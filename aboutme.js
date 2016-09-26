ddff8271f4e2150a8f186d7c8560da64bbacfe22
gr(three)bb(zero) <gr3bb0@gmail.com>
2016-09-26 14:10:04 -0600

    Add files via upload

--- aboutme.js
+++ aboutme.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
 	$("#aboutme").click(function() {
 		$("#myinfo").fadeToggle(500);
 	});
+	// start of mouseover
+	$(".mousechange").mouseenter(function() {
+		$(this).css("color", "#0000ff");
+	});
+	$(".mousechange").mouseleave(function() {
+		$(this).css("color", "#ffffff");
+	});
 });
